f498a8c16dbad331820018b102cf837ff7ba3456
Patricia Severance <patriciakseverance@gmail.com>
2023-02-24 16:35:27 -0700

    Refactor recipes_controller

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,13 +1,7 @@
 class Api::V1::RecipesController < ApplicationController
-  # def index
-  # #  require 'pry'; binding.pry
-  #   recipe = RecipeFacade.all_recipes(params[:id])
-  #   render json: RecipeSerializer.new(recipe)
-  # end
-
   def search
-    recipe_info = RecipeFacade.search_recipe(params[:ingredients])
-    render json: RecipeSerializer.new(recipe_info)
+    recipe_info = DiscoverFacade.search_recipe(params[:ingredients])
+    render json: DiscoverSerializer.new(recipe_info)
   end
 
   def show
