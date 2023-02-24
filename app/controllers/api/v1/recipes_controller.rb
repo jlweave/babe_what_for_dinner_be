4f5c978796020b70e4fcff2a7b4df9960f6ebf0b
Lacey Weaver <jlweave89@gmail.com>
2023-02-24 09:22:44 -0700

    Add completed show method

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,12 +1,18 @@
 class Api::V1::RecipesController < ApplicationController
-  def index
+  # def index
+  # #  require 'pry'; binding.pry
+  #   recipe = RecipeFacade.all_recipes(params[:id])
+  #   render json: RecipeSerializer.new(recipe)
+  # end
+
+  def search
     recipe_info = RecipeFacade.search_recipe(params[:ingredients])
     render json: RecipeSerializer.new(recipe_info)
   end
   
   def show
-    #  query = params[:q]
-    # term = RecipeFacade(query)
-    render json: RecipeSerializer.new(Recipe.find(params[:id]))
+    query = params[:id]
+    recipe = RecipeFacade.single_recipe(query)
+    render json: RecipeSerializer.new(query)
   end
 end
--- spec/requests/api/v1/recipes_request_spec.rb
+++ spec/requests/api/v1/recipes_request_spec.rb
@@ -7,9 +7,15 @@ describe "Recipe API" do
     stub_request(:get, "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=apples%2Cflour%2Csugar&number=200&ignorePantry=true&ranking=1")
       .with(headers: { 'X-RapidAPI-Key' => '4f4fbc3ba2msh745c24cf4e7edbdp13621djsn50bc1816f418', 'X-RapidAPI-Host' => 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com' })
       .to_return(status: 200, body: json_response, headers: {})
+
+ json_response = File.read('spec/fixtures/single_recipe.json')
+    stub_request(:get, "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/479101/information")
+      .with(headers: { 'X-RapidAPI-Key' => '4f4fbc3ba2msh745c24cf4e7edbdp13621djsn50bc1816f418', 'X-RapidAPI-Host' => 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com' })
+      .to_return(status: 200, body: json_response, headers: {})
   end
-  it "returns recipes and the attr" do
-    get '/api/v1/recipes?ingredients=apples,flour,sugar'
+ 
+  it "returns recipes that contain ingrediants search by(findByIngredients)" do
+    get '/api/v1/recipes/findByIngredients?ingredients=apples,flour,sugar'
 
     expect(response).to be_successful
     recipes = JSON.parse(response.body, symbolize_names: true)
@@ -27,5 +33,9 @@ describe "Recipe API" do
     expect(recipe[:attributes]).to have_key(:ingredient)
     expect(recipe[:attributes][:ingredient]).to be_a(Array)
     end
+  end
+
+  it "returns a recipes show page" do
+    get "/api/v1/recipes/479101/information"
   end
 end
