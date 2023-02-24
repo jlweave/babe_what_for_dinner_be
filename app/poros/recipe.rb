e6614932a2880fcd119e90adec129e264f7725d6
Patricia Severance <patriciakseverance@gmail.com>
2023-02-24 16:35:01 -0700

    Refactor recipe poro

--- app/poros/recipe.rb
+++ app/poros/recipe.rb
@@ -1,10 +1,23 @@
 class Recipe
-  attr_reader :title, :ingredients, :image, :recipe_id
-  
-  def initialize(attr)
-    @title = attr[:title]
-    @ingredients = attr[:extendedIngredients].map {|ingredient| ingredient[:nameClean]}
-    @image = attr[:image]
-    @recipe_id = attr[:id]
+  attr_reader :id, :title, :ready_in_minutes, :image, :summary, :instructions, :ingredients
+  def initialize(data)
+    # binding.pry
+    @id = data[:id]
+    @title = data[:title]
+    @ready_in_minutes = data[:readyInMinutes]
+    @image = data[:image]
+    @summary = data[:summary]
+    @instructions = data[:instructions]
+    @ingredients = extended_ingredients(data[:extendedIngredients])
+  end
+
+  def extended_ingredients(data)
+    data.map do |ingr|
+      {
+        :name => ingr[:name],
+        :amount => ingr[:amount],
+        :unit => ingr[:unit]
+      }
+    end
   end
 end
