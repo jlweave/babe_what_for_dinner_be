14feca9e09a437eee5fbb167984c818ec449b50e
Patricia Severance <patriciakseverance@gmail.com>
2023-02-27 14:28:54 -0700

    remove prys from service

--- app/facade/recipe_facade.rb
+++ app/facade/recipe_facade.rb
@@ -1,11 +1,4 @@
 class RecipeFacade 
-  # def self.all_recipes(term)
-  #   data = RecipeService.find_all_recipe(term)
-  #   data.map do |recipe_attr|
-  #     Recipe.new(recipe_attr)
-  #   end
-  # end
-  
   def self.single_recipe(term)
     data = RecipeService.get_recipe(term)
     Recipe.new(data)
