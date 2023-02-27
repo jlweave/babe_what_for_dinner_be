84971d2e465ecb9a50d7e13935c7f76c5f271960
Ryan Canton <ryancanton999@gmail.com>
2023-02-27 13:10:04 -0700

    Refactor: removed uneccessary require statement

--- spec/requests/api/v1/user_recipes_request_spec.rb
+++ spec/requests/api/v1/user_recipes_request_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe "User Recipes Favorites" do
   end
 
   it '' do
-    get '/api/v1/user_recipes'
+    post '/api/v1/user_recipes?uid=1234&recipe_id=12345&recipe_name=applesauce'
+    binding.pry
   end
 end
