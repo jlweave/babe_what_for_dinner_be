0a48b6ddfbde6507597edd71b8fc0d3532f37aef
Bryan Flanagan <bflanagan78@yahoo.com>
2023-02-28 14:40:06 -0700

    test: Add test for missing_ingredients method, test passes

--- spec/poros/discover_spec.rb
+++ spec/poros/discover_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "discover_result" do
       image: "https://wwwflickeringmythc3c8f7.zapwp.com/q:i/r:0/wp:1/w:600/u:https://cdn.flickeringmyth.com/wp-content/uploads/2019/07/YoungEinstein_0002-600x391.jpg",
       title: "Stir Fry Broccoli",
       missedIngredientCount: 1489,
-      missedIngredients: [ {name: "soy sauce", amount: 1.0, unit: "cup"}, {name: "Sesame Seets", amount: 2.2, unit: "oz"} ]
+      missedIngredients: [ {name: "soy sauce", amount: 1.0, unit: "cup"}, {name: "Sesame Seeds", amount: 2.2, unit: "oz"} ]
     }
     result = DiscoverResult.new(data)
 
@@ -15,5 +15,13 @@ RSpec.describe "discover_result" do
     expect(result.image).to be_a String
     expect((result.missing_ingr).count).to eq 2
     expect(result.missing_ingr_count).to eq 1489
+    expect(result.missing_ingredients(data[:missedIngredients])).to be_a Array
+    result.missing_ingredients(data[:missedIngredients]).each do |ingredient|
+      expect(ingredient.count).to eq 3
+      expect(ingredient).to be_a Hash
+      expect(ingredient[:name]).to be_a String
+      expect(ingredient[:amount]).to be_a Float
+      expect(ingredient[:unit]).to be_a String
+    end
   end
 end
